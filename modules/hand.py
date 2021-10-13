cd6430d35a727fb59b290d9e5a81b7ded97a0977
jesusgodinho59a <jesus.godinho@59a.co.uk>
2021-10-13 15:41:21 +0100

    Added documentation and new  in hand.py

--- modules/hand.py
+++ modules/hand.py
@@ -4,6 +4,7 @@ class Hand:
         * Add                   - Add cards to the card
         * Get_cards             - Returns all cards in the player hand
         * Transfer_card         - Moves / Transfers a given card from the player hand to the main deck
+        * Show_cards            - Displays / Prints all the cards in a player's hand
     
     Dependencies:
         * None
@@ -25,3 +26,14 @@ class Hand:
     def transfer_card(self,card,deck):
         self.__cards.remove(card)
         deck.add(card)
+
+    #Shows cards the player currently has
+    def show_cards(self):
+        player_cards = self.get_cards()
+        print("Card count : ",len(player_cards))
+        print("Your cards -> \n")
+
+        for i in range(0,len(player_cards)):
+            card = player_cards[i]
+            card_color, card_type, card_id = card.get_properties()
+            print(str(i+1)+". "+card_id)
